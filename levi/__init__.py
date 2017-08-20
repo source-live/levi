b1a101ce293500231742a118396b51e0b5ffc1a4
Zack P <zack@zacklearns.com>
2017-08-20 12:45:44 -0600

    Create __init__.py

---
+++ levi/__init__.py
@@ -0,0 +1,46 @@
+import turtle
+
+def go_right(step):
+     turtle.setheading(0)
+     turtle.forward(step)
+
+def go_up(step):
+     turtle.setheading(90)
+     turtle.forward(step)
+
+
+def go_left(step):
+     turtle.setheading(180)
+     turtle.forward(step)
+
+def go_down(step):
+     turtle.setheading(270)
+     turtle.forward(step)
+
+def levi_draw(step_size,step_number):
+    move_dict = {1: go_up,
+                 2: go_right,
+                 3: go_left,
+                 4: go_down
+                 }
+    for _ in range(step_number):
+        move_in_a_direction = move_dict[random.randint(1, 4)]
+        move_in_a_direction(step_size)
+        
+        
+        
+class Art:
+  def __init__(step_size=1,step_number=100):
+    levi_draw(step_size,step_number)
+  
+  def go_up(step=1):
+    go_up(step)
+    
+  def go_down(step=1):
+    go_down(step)
+  
+  def go_right(step=1):
+    go_right(step)
+    
+  def go_left(step=1):
+    go_right(step)
